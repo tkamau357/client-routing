6440c9b6550be0d9456b61bb74a11676bcccc68e
melvinjames <melvin.gacheru@student.morigaschool.com>
2023-04-25 12:23:30 +0300

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import ContactForm from './components/ContactForm';
 
 function App() {
   return (
     <div className="App">
+      <ContactForm/>
       <header className="App-header">
         
       </header>
---
+++ src/components/ContactForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ContactForm (){
+  return <H1>THIS IS OUR CONTACT FORM </H1>
+}
+
+export default ContactForm;
