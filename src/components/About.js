156ec8edd38ff5f66284f633bd5ef334f48d6629
Jashin254 <joshuanissi254@gmail.com>
2023-04-25 12:25:27 +0300

    about-component

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function about(){
+    return(
+        <div className="aboutnav">
+            <h5> ABOUT US</h5>
+            <p>This is a simple routing of components </p>
+        </div>
+    )
+}
+export default about;
